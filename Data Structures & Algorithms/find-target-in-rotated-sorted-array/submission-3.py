94fb80da2bd9ae0c5e080d1725bbc9d46a4895dd
Krish <95929018+krishv24@users.noreply.github.com>
2026-06-25 13:07:31 +0530

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,34 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left=0
+        right=len(nums)-1
+        minimum=9999999
+        mid=(left+right)//2
+        if nums[mid] < minimum:
+            minimum=nums[mid]
+            print("minimum",minimum)
+        if nums[mid]==target:
+                return mid
+        def binsearch(nums,left,right):
+            mid = (left + right) // 2
+
+            if left > right:
+                return -1
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[left] <= nums[mid]:
+
+                if nums[left] <= target < nums[mid]:
+                    return binsearch(nums, left, mid - 1)
+                else:
+                    return binsearch(nums, mid + 1, right)
+
+            else:
+
+                if nums[mid] < target <= nums[right]:
+                    return binsearch(nums, mid + 1, right)
+                else:
+                    return binsearch(nums, left, mid - 1)
+        return binsearch(nums,left,right)
